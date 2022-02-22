20a831841dd59a7e70aee0cee64c8c7844cd2b8d
Amit Reddy Satyaram <amit.satyaram@gmail.com>
2022-02-22 10:56:23 +0530

    Second commit

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 
 const app = express();
+console.log(md5("123"));
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
@@ -19,7 +22,6 @@ email: String,
 password: String
 
 });
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User",userSchema)
 
@@ -40,7 +42,7 @@ app.get("/register",function(req,res){
  app.post("/register",function(req,res){
 const newUser = new User({
   email:req.body.username,
-  password:req.body.password
+  password:md5(req.body.password)
 });
   newUser.save(function(err){
     if(err)
@@ -53,7 +55,7 @@ const newUser = new User({
 
 app.post("/login",function(req,res){
 const username = req.body.username;
-const password  =req.body.password;
+const password  =md5(req.body.password);
 
   User.findOne({email:username},function(err, foundUser){
   if(err)
